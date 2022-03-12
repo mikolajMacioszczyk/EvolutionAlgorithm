b0b489bfa8f93f6adbfcc0c6226a8db85908b4f4
Mikołaj <macioszczyk.mikolaj@gmail.com>
2022-03-12 12:36:37 +0100

    implement move top/down

--- Lista1/Operators/Mutation/RectangleMovementMutation.cs
+++ Lista1/Operators/Mutation/RectangleMovementMutation.cs
@@ -1,7 +1,7 @@
 ﻿using Lista1.Interfaces;
 using Lista1.Models;
 
-namespace Lista1.Operators.Mutation
+namespace Lista1.Operators
 {
     public class RectangleMovementMutation : IMutationOperator
     {
@@ -19,28 +19,34 @@ namespace Lista1.Operators.Mutation
 
         public void Mutate(Member member)
         {
-            // wybierz rozmiary prostokąta
+            // choose rectangle dimensions
             var xLength = random.Next(1, _dimX - 1);
             var yLength = random.Next(1, _dimY - 1);
 
-            // wybierz położenie lewego rogu (tak żeby się mieścił na planszy)
-            var topOffset = random.Next(0, _dimX - xLength);
-            var leftOffset = random.Next(0, _dimY - yLength);
+            // choose left corner coordinates
+            var topOffset = random.Next(0, _dimX - xLength + 1);
+            var leftOffset = random.Next(0, _dimY - yLength + 1);
 
-            switch (random.Next(0, 4))
+            // Temp 2
+            switch (random.Next(2))
             {
                 case 0:
                     MoveTop(member, xLength, yLength, topOffset, leftOffset);
                     break;
                 case 1:
-                    // w prawo
+                    MoveDown(member, xLength, yLength, topOffset, leftOffset);
                     break;
                 case 2:
-                    // w dół
+                    // w praw
                     break;
                 case 3:
                     // w lewo
                     break;
+            }
+
+            if (!member.IsValid(24))
+            {
+
             }
         }
 
@@ -51,14 +57,61 @@ namespace Lista1.Operators.Mutation
                 return; 
             }
 
-            var move = random.Next(1, topOffset);
+            int move = random.Next(topOffset) + 1;
 
-            for (int i = leftOffset; i < leftOffset + yLength; i++)
+            var tempRow = new int[move, yLength];
+            for (int j = 0; j < move; j++)
             {
-                for (int j = 0; j < xLength; j++)
+                for (int i = 0; i < yLength; i++)
                 {
-
+                    tempRow[j, i] = member[topOffset - move + j, leftOffset + i];
                 }
+            }
+
+            for (int i = topOffset; i < topOffset + xLength; i++)
+            {
+                for (int j = leftOffset; j < leftOffset + yLength; j++)
+                {
+                    member[i - move, j] = member[i, j];
+                }
+            }
+
+            for (int j = 0; j < move; j++)
+            {
+                for (int i = 0; i < yLength; i++)
+                {
+                    member[topOffset - move + j + xLength, leftOffset + i] = tempRow[j, i];
+                }
+            }
+        }
+
+        private void MoveDown(Member member, int xLength, int yLength, int topOffset, int leftOffset)
+        {
+            int nextRow = topOffset + xLength;
+            if (nextRow >= _dimX)
+            {
+                return;
+            }
+
+            int move = random.Next(_dimX - nextRow) + 1;
+
+            var tempRow = new int[yLength];
+            for (int i = 0; i < yLength; i++)
+            {
+                tempRow[i] = member[nextRow, leftOffset + i];
+            }
+
+            for (int i = nextRow; i > topOffset; i--)
+            {
+                for (int j = leftOffset; j < leftOffset + yLength; j++)
+                {
+                    member[i, j] = member[i - 1, j];
+                }
+            }
+
+            for (int i = 0; i < yLength; i++)
+            {
+                member[topOffset, leftOffset + i] = tempRow[i];
             }
         }
     }
